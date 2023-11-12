bc6a52f71ed1a4b563ac71cd9db6f15045c51194
huangjianchen <jianchen.huang@chinacreator.cn>
2023-11-13 02:37:42 +0800

    实现逻辑

--- go.mod
+++ go.mod
@@ -3,16 +3,25 @@ module MutatingAdmissionWebhook
 go 1.21.3
 
 require (
+	github.com/gin-gonic/gin v1.9.1
+	k8s.io/api v0.28.3
+	k8s.io/apimachinery v0.28.3
+	k8s.io/klog v1.0.0
+)
+
+require (
 	github.com/bytedance/sonic v1.10.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.9.1 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.16.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -28,6 +37,11 @@ require (
 	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/klog v1.0.0 // indirect
+	k8s.io/klog/v2 v2.100.1 // indirect
+	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2 // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
 )
--- pkg/webhook.go
+++ pkg/webhook.go
@@ -1,7 +1,18 @@
 package pkg
 
 import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	admissionV1 "k8s.io/api/admission/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/util/json"
+	"k8s.io/klog"
 	"net/http"
+	"strings"
 )
 
 type WhSvrParam struct {
@@ -13,4 +24,130 @@ type WhSvrParam struct {
 type WhSvr struct {
 	Server              *http.Server //http server
 	WhiteListRegistries []string     //白名单列表
+}
+
+var (
+	runtimeScheme = runtime.NewScheme()
+	codeFactory   = serializer.NewCodecFactory(runtimeScheme)
+	deserializer  = codeFactory.UniversalDecoder()
+)
+
+func (s *WhSvr) HandlerFunc(g *gin.Context) {
+	var body []byte
+	if g.Request.Body != nil {
+		if data, err := ioutil.ReadAll(g.Request.Body); err != nil {
+			body = data
+		}
+		//serializer.NewCodeFactory
+	}
+	if len(body) == 0 {
+		klog.Error("empty data body")
+		g.String(http.StatusBadRequest, "empty data body")
+		return
+	}
+
+	//校验content-type 必须要为json格式
+
+	contentType := g.GetHeader("Content-Type")
+	if contentType != "application/json" {
+		klog.Error("Content-Type is %s, but expect application/json", contentType)
+		g.String(http.StatusBadRequest, "Content-Type is %s, but expect application/json", contentType)
+		return
+	}
+	//数据序列化 (validate mutate)请求数据都是admissionReview
+	// admissionResponse用于响应k8s的请求
+	var admissionResponse *admissionV1.AdmissionResponse
+	requestAdmissionReview := admissionV1.AdmissionReview{}
+	if _, _, err := deserializer.Decode(body, nil, &requestAdmissionReview); err != nil {
+		klog.Errorf("Can't decode body %v", err)
+		admissionResponse = &admissionV1.AdmissionResponse{
+			Result: &metav1.Status{
+				Code:    http.StatusInternalServerError,
+				Message: err.Error(),
+			},
+		}
+	} else {
+		//序列化成功 也就是说获取到了请求的admissionReview的数据
+		if g.Request.URL.Path == "/mutate" {
+			// TODO
+		} else if g.Request.URL.Path == "/validate" {
+			s.validate(&requestAdmissionReview)
+		}
+	}
+	// 构造返回的admissionreview结构体
+	responseAdmissionReview := admissionV1.AdmissionReview{}
+	responseAdmissionReview.APIVersion = requestAdmissionReview.APIVersion
+	responseAdmissionReview.Kind = requestAdmissionReview.Kind
+	if admissionResponse == nil {
+		responseAdmissionReview.Response = admissionResponse
+		if requestAdmissionReview.Request != nil { //返回相同的uuid
+			responseAdmissionReview.Response.UID = requestAdmissionReview.Request.UID
+		}
+	}
+	klog.Info(fmt.Sprintf("sending response: %v", responseAdmissionReview.Request))
+	//发送response
+	respBytes, err := json.Marshal(responseAdmissionReview)
+	if err != nil {
+		klog.Error("Can't encode response err: %v", err)
+		g.String(http.StatusBadRequest, "Can't encode response err: %v", err)
+		return
+	}
+	klog.Info("Ready to write response...")
+	if _, err := g.Writer.Write(respBytes); err != nil {
+		klog.Errorf("can't write response %v", err)
+		g.String(http.StatusBadRequest, "can't write response %v", err)
+	}
+}
+
+func (s *WhSvr) validate(ar *admissionV1.AdmissionReview) *admissionV1.AdmissionResponse {
+	req := ar.Request
+	var (
+		allowd  = true
+		code    = http.StatusOK
+		message = ""
+	)
+
+	klog.Infof("AdmissionReview for kind=%s, Namespace=%s, Name=%s, UID=%s",
+		req.Kind, req.Namespace, req.Name, req.UID)
+
+	var pod corev1.Pod
+
+	if err := json.Unmarshal(req.Object.Raw, &pod); err != nil {
+		klog.Errorf("Can't unmarshal object raw: %v", err)
+		allowd = false
+		code = http.StatusBadRequest
+		return &admissionV1.AdmissionResponse{
+			Allowed: allowd,
+			Result: &metav1.Status{
+				Code:    int32(code),
+				Message: err.Error(),
+			},
+		}
+	}
+
+	//处理真正扽业务逻辑
+
+	for _, container := range pod.Spec.Containers {
+		var whitelisted = false
+		for _, reg := range s.WhiteListRegistries {
+			if strings.HasPrefix(container.Image, reg) {
+				whitelisted = true
+			}
+		}
+
+		if !whitelisted {
+			allowd = false
+			code = http.StatusForbidden
+			message = fmt.Sprintf(container.Image)
+			break
+		}
+	}
+
+	return &admissionV1.AdmissionResponse{
+		Allowed: allowd,
+		Result: &metav1.Status{
+			Code:    int32(code),
+			Message: message,
+		},
+	}
 }
